72d00cec4dbb1126aadf24c2af5f307c9c503dab
Dani Quesada <daniela.dudyfit@gmail.com>
2023-09-24 20:44:28 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+
+import telebot
+from dotenv import load_dotenv
+from handlers import *
+
+
+def main():
+    load_dotenv()
+
+    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+    if BOT_TOKEN:
+        bot = telebot.TeleBot(BOT_TOKEN)
+        
+        initBot(bot,61695)
+        print("initiated bot")
+        bot.infinity_polling()
+    else:
+        print('Error with BOT INIT')
+
+
+if __name__ == '__main__':
+    main()
